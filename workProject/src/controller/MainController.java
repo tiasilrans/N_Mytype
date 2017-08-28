269395b38dd7e0dc79fe90a5bbae1fec1ed30fcb
1103-20 <1103-20@1103-20-PC>
2017-08-28 16:00:56 +0900

    보라 - 타일즈 설정 / 메인 화면

--- workProject/src/controller/MainController.java
+++ workProject/src/controller/MainController.java
@@ -1,8 +1,18 @@
 package controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class MainController {
+	
+	@RequestMapping({ "/", "/index.mt" })
+	public String welcome(Map map) {
+		map.put("section", "main");
+		return "t_el";
+	}
 
 }
