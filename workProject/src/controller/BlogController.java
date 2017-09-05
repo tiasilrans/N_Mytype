227a446ebaad413eacc7a8751a4fc1129e611810
1103-20 <1103-20@1103-20-PC>
2017-09-05 19:18:34 +0900

    보라 - 카테고리 수정

--- workProject/src/controller/BlogController.java
+++ workProject/src/controller/BlogController.java
@@ -81,8 +81,9 @@ public class BlogController {
 	@RequestMapping("/{url}/categories")
 	public ModelAndView categories(@PathVariable(value="url") String url){
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("t_el");
-	 	mav.addObject("section", "blog/categories");
+			mav.setViewName("t_el");
+			mav.addObject("section", "blog/categories");
+	 		mav.addObject("url", url);
 		return mav;
 		
 	}
